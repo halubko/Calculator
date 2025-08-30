6fd425ad503de42233cc63b7589daa447bf44999
halubko <misha.golubko@gmail.com>
2025-08-30 21:34:55 +0300

    added functionality for operator buttons

---
+++ src/algorithms.js
@@ -0,0 +1,17 @@
+export function algorithm(num1, num2, operation) {
+   num1 = Number(num1)
+   num2 = Number(num2)
+
+   switch (operation) {
+      case "add":
+         return num1 + num2
+      case "subtract":
+         return num1 - num2
+      case "multiply":
+         return num1 * num2
+      case "divide":
+         return num2 !== 0 ? num1 / num2 : "Divide on zero"
+      default:
+         return "Unknown operation"
+   }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./assets/styles/styles.scss"
+import { algorithm } from "./algorithms"
 
 //Input(text) validation
 document.getElementById("output").addEventListener("input", (e) => {
@@ -22,3 +23,43 @@ document.querySelectorAll(".input_btn").forEach((btn) =>
       }
    })
 )
+
+//AC button
+document.getElementById("clean").addEventListener("click", () => {
+   document.getElementById("output").value = "0"
+   document.querySelectorAll(".functional").forEach((btn) => {
+      btn.removeAttribute("id")
+   })
+})
+
+//Functional buttons
+document.querySelectorAll(".functional").forEach((btn) => {
+   btn.addEventListener("click", () => {
+      const output = document.getElementById("output")
+      const operator = btn.getAttribute("operator")
+
+      if (operator !== "equals") {
+         document.querySelectorAll(".functional").forEach((past) => {
+            past.removeAttribute("id")
+         })
+         btn.id = "active"
+         btn.setAttribute("number1", output.value)
+         output.value = "0"
+      }
+
+      if (operator === "equals") {
+         const active_btn = document.getElementById("active")
+         if (active_btn) {
+            const num1 = active_btn.getAttribute("number1")
+            const selected_operator = active_btn.getAttribute("operator")
+
+            output.value = algorithm(num1, output.value, selected_operator)
+
+            document.querySelectorAll(".functional").forEach((btn) => {
+               btn.removeAttribute("id")
+               btn.removeAttribute("number1")
+            })
+         }
+      }
+   })
+})
